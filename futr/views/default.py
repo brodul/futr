4e8f1866d3547b8a023f19fb1b4a18eb1e81cd1f
Andraz Brodnik <brodul@brodul.org>
2017-01-28 23:50:47 +0100

    Refactor pivnica parser

--- futr/views/default.py
+++ futr/views/default.py
@@ -1,25 +1,13 @@
-import re
-from datetime import datetime
-
 from pyramid.view import view_config
-from bs4 import BeautifulSoup
-import requests
 
-non_decimal = re.compile(r'[^\d.]+')
+import futr.parsers
 
 
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
 def my_view(request):
-    response = requests.get("http://www.pivnica-union.si/si/")
-    soup = BeautifulSoup(response.text, 'html.parser')
-    nom = soup.find(class_="foodDayMenu")
-    nom = nom.findAll(class_=["foodDayMenuBlockTitle", "foodDayMenuBlock"])
-    # import ipdb; ipdb.set_trace()
-    for raw_date, meni in zip(nom[::2], nom[1::2]):
-        raw_date = raw_date.text.strip("-").strip()
-        str_date = non_decimal.sub('', raw_date)
-        datetime_object = datetime.strptime(str_date, '%d.%m.%Y')
-        if datetime_object.date() == datetime.today():
-            break
-
-    return {'union': ((raw_date, meni),)}
+    pivnica_parser = futr.parsers.PivnicaParser()
+    try:
+        pivnica_content = pivnica_parser.get_day_content()
+    except futr.parsers.NoMenuForTodayError:
+        pivnica_content = ""
+    return {'union': pivnica_content}
