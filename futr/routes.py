1bc1b7011ef982c833c85ac6fd3d2a75a8e59abf
Andraz Brodnik <brodul@brodul.org>
2017-10-05 16:26:04 +0200

    Add api

--- futr/routes.py
+++ futr/routes.py
@@ -4,6 +4,7 @@ from futr.services.database.factory import db
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('home_json', '/api/')
     config.add_tween('futr.routes.close_db_tween_factory')
 
 
--- futr/views/default.py
+++ futr/views/default.py
@@ -26,3 +26,26 @@ def my_view(request):
     locations = person_location_c.get_locations()
     return {'union': pivnica_content, 'piramida': piramida_content,
         'locations': locations, 'person_locations': person_location}
+
+@view_config(route_name='home_json', renderer='json')
+def my_view_(request):
+    person_location_c = PersonLocationController()
+    if request.method == 'POST' and request.POST['name']:
+        person = request.POST['name']
+        location = request.POST['locationSelect']
+        person_location_c.create_person_location(person, location)
+
+    pivnica_aggregator = futr.parsers.PivnicaAggregator()
+    try:
+        pivnica_content = pivnica_aggregator.get_day_content()
+    except futr.parsers.NoMenuForTodayError:
+        pivnica_content = ""
+    try:
+        piramida_aggregator = futr.parsers.PiramidaAggregator()
+        piramida_content = piramida_aggregator.get_day_content()
+    except (futr.parsers.NoMenuForTodayError, ValueError):
+        piramida_content = ""
+    person_location = person_location_c.get()
+    locations = person_location_c.get_locations()
+    return {'union': repr(pivnica_content), 'piramida': repr(piramida_content),
+        'locations': locations, 'person_locations': person_location}
