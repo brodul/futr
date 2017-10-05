b9d99fe5baae2872f222dfb82ba974169302fef5
Andraz Brodnik <brodul@brodul.org>
2017-10-05 16:00:06 +0200

    Add validator

--- futr/views/default.py
+++ futr/views/default.py
@@ -7,7 +7,7 @@ from futr.controller.person_location import PersonLocationController
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
 def my_view(request):
     person_location_c = PersonLocationController()
-    if request.method == 'POST':
+    if request.method == 'POST' and request.POST['name']:
         person = request.POST['name']
         location = request.POST['locationSelect']
         person_location_c.create_person_location(person, location)
